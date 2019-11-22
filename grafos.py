bf8d64dd4a8a11cc2ce2d83647fb6a0e5e19fe24
James Yuri <jamesventosa@hotmail.com>
2019-11-21 23:40:00 -0300

    grafo basico implementado

---
+++ grafos.py
@@ -0,0 +1,81 @@
+class Grafo(object):
+    def __init__(self, grafo=None):
+        if grafo == None:
+            grafo = {}
+        self.__grafo = grafo
+
+    def vertices(self):
+        return list(self.__grafo())
+
+    def arestas(self):
+        return self.__arestas_grafo()
+
+    def add_vertice(self, vertice):
+        if vertice not in self.__grafo:
+            self.__grafo[vertice] = []
+    
+    def add_aresta(self, aresta):
+        aresta = set(aresta)
+        verticeA = aresta.pop()
+        if aresta:
+            verticeB = aresta.pop()
+        else:
+            # loop
+            verticeB = verticeA
+        if verticeA in self.__grafo:
+            self.__grafo[verticeA].append(verticeB)
+        else:
+            self.__grafo[verticeA] = [verticeB]
+
+
+
+    def __arestas_grafo(self):
+        arestas = []
+        for vertice in self.__grafo:
+            for vizinho in self.__grafo[vertice]:
+                if {vizinho, vertice} not in arestas:
+                    arestas.append({vertice, vizinho})
+        return arestas
+
+    
+
+    def __str__(self):
+        res = "Vertices: "
+        for k in self.__grafo:
+            res += str(k) + " "
+        res += "\Arestas: "
+        for edge in self.__arestas_grafo():
+            res += str(edge) + " "
+        return res
+
+    def vertice_isolado(self):
+        grafo = self.__grafo
+        isolados = []
+        for vertice in grafo:
+            print(isolados, vertice)
+            if not grafo[vertice]:
+                isolados += [vertice]
+        return isolados
+    
+    def eh_conexo(self, vertices_encontrados = None, vertice_inicio=None):
+        if vertices_encontrados is None:
+            vertices_encontrados = set()
+        gdict = self.__grafo        
+        vertices = list(gdict.keys()) 
+        if not vertice_inicio:
+            vertice_inicio = vertices[0]
+        vertices_encontrados.add(vertice_inicio)
+
+
+
+
+g = { "va" : ["d"],
+      "vb" : ["c"],
+      "vc" : ["b", "c", "d", "e"],
+      "vd" : ["a", "c"],
+      "ve" : ["c"],
+      "vf" : []
+    }
+
+grafo = Grafo(g)
+print(grafo)
